5bceeb551058d83eac25b827ca17ec58b519688d
josephprasanth <prasanthjoseph_77@yahoo.com>
2025-11-07 10:03:18 +0100

    Test commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LCart
+
 A modular React + Mui Cart system built with useReducer and Context.
+
+# To Run Locally
+
+npm install
+npm start
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+function App() {
+  return <Typography variant="h4">Roboto Test</Typography>;
+}
+
+export default App;
